bda8745853e3441e2b3008481a6836e05045f7cf
Sorawit255 <65030255@kmitl.ac.th>
2024-04-14 11:31:10 +0700

    onSubnit event

--- react-1/src/component/Addform.js
+++ react-1/src/component/Addform.js
@@ -2,9 +2,24 @@ import "./Addform.css";
 import { useState } from "react";
 export default function AddForm() {
     const [name,setname]=useState("");
+    function saveStudent(e){
+        e.preventDefault();
+        console.log(name);
+        if(!name){
+            alert("ป้อนชื่อ!!")
+        }
+        else{
+            const newStudent={
+                ID:Math.floor(Math.random()*1000),
+                name:name
+            }
+            console.log(newStudent);
+            setname("")
+        }
+    }
   return (
     <section className="container">
-      <form>
+      <form onSubmit={saveStudent}>
         <label>Student Name</label>
         <input type="text" name="name" value={name} onChange={(e)=>setname(e.target.value)}/>
         <button type="submit" className="btn-add">บันทึก</button>
