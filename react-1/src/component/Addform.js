81e9cb4ee9595c54b516ad95f1a97d626e9b91d2
Sorawit255 <65030255@kmitl.ac.th>
2024-04-14 11:24:21 +0700

    onChange event

--- react-1/src/component/Addform.js
+++ react-1/src/component/Addform.js
@@ -1,10 +1,12 @@
 import "./Addform.css";
+import { useState } from "react";
 export default function AddForm() {
+    const [name,setname]=useState("");
   return (
     <section className="container">
       <form>
         <label>Student Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={(e)=>setname(e.target.value)}/>
         <button type="submit" className="btn-add">บันทึก</button>
       </form>
     </section>
