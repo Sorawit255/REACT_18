288c8fd6fd8d675442e7e46507428200bbe45028
Sorawit255 <65030255@kmitl.ac.th>
2024-04-14 22:10:59 +0700

    สร้างและแสดงข้อมูลใน state

--- react-app1/src/App.js
+++ react-app1/src/App.js
@@ -2,14 +2,23 @@ import "./App.css";
 import Header from './components/Header';
 import AddForm from './components/AddForm';
 import Item from './components/Item';
-
+import {useState } from "react";
 function App() {
+  const [tasks,setTasks]=useState([
+    {id:1,title:"sdgsdg"}
+  ])
   return (
     <div className="App">
       <Header/>
       <div className="container">
         <AddForm/>
-        <Item/>
+        <section>
+          {
+            tasks.map((data)=>(
+              <Item key={data.id} data={data}/>
+            ))
+          }
+        </section>
       </div>
     </div>
   );
