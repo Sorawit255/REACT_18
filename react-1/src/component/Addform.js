4d80bab2a90a0e5cb2b16391dd94327be585a40d
Sorawit255 <65030255@kmitl.ac.th>
2024-04-14 12:05:23 +0700

    ใช้ select

--- react-1/src/component/Addform.js
+++ react-1/src/component/Addform.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function AddForm(props) {
     const {students,setstudents}=props;
     const [name,setname]=useState("");
+    const [gender,setGender]=useState("male");
     function saveStudent(e){
         e.preventDefault();
         if(!name){
@@ -10,7 +11,8 @@ export default function AddForm(props) {
         }else{
             const newStudent={
                 ID:Math.floor(Math.random()*1000),
-                name:name
+                name:name,
+                gender:gender
             }
             setstudents([...students,newStudent])
             setname("")
@@ -21,6 +23,10 @@ export default function AddForm(props) {
       <form onSubmit={saveStudent}>
         <label>Student Name</label>
         <input type="text" name="name" value={name} onChange={(e)=>setname(e.target.value)}/>
+        <select value={gender} onChange={(e)=>setGender(e.target.value)}>
+            <option value="male">male</option>
+            <option value="female">female</option>
+        </select>
         <button type="submit" className="btn-add">บันทึก</button>
       </form>
     </section>
