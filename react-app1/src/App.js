df3fb3064bcfd3b526b8039a90deba87b3210012
Sorawit255 <65030255@kmitl.ac.th>
2024-04-15 21:36:45 +0700

    บันทึกข้อมูล

--- react-app1/src/App.js
+++ react-app1/src/App.js
@@ -5,18 +5,33 @@ import Item from './components/Item';
 import {useState } from "react";
 function App() {
   const [tasks,setTasks]=useState([
-    {id:1,title:"sdgsdg"}
+    {id:1,title:"sdgsdg"},
   ])
   const [title,setTitle]=useState("")
   function deleteTask(id){
     const result = tasks.filter(item=>item.id !==id)
     setTasks(result)
   }
+  function saveTask(e){
+    e.preventDefault();
+    if(!title){
+      alert("กรุณาป้อนข้อมูลด้วยครับ")
+    }
+    else{
+      //เพิ่มรายการใหม่
+      const newTask={
+          id:Math.floor(Math.random()*1000),
+          title:title
+      }
+      setTasks([...tasks,newTask])
+      setTitle("")
+    }
+  }
   return (
     <div className="App">
       <Header/>
       <div className="container">
-        <AddForm title={title} setTitle={setTitle}/>
+        <AddForm title={title} setTitle={setTitle} saveTask={saveTask}/>
         <section>
           {
             tasks.map((data)=>(
--- react-app1/src/components/AddForm.js
+++ react-app1/src/components/AddForm.js
@@ -1,10 +1,10 @@
 import "./AddForm.css";
 export default function AddForm(props){
-    const { title, setTitle } = props
+    const { title, setTitle ,saveTask} = props
     return(
         <>
             <h2>แอพบริหารจัดการงาน</h2>
-            <form>
+            <form onSubmit={saveTask}>
                 <div className="form-control">
                     <input typr="text" className="text-input" value={title} onChange={(e)=> setTitle(e.target.value)}/>
                     <button type="submit"className="submit-btn">Add</button>
