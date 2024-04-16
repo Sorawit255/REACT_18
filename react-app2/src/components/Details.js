9362c32efc9308a8ea67d0971d43b502b9882033
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 22:44:32 +0700

    แสดงรายละเอียดบทความ

--- react-app2/src/components/Details.js
+++ react-app2/src/components/Details.js
@@ -14,10 +14,16 @@ export default function Details(){
         setImage(result.image_url)
         setContent(result.content)
         setAuthor(result.author)
-    })
+        // eslint-disable-next-line
+    },[])
     return(
        <div className="container">
-            <h2 className="title">บทความ : {id}</h2>
+            <h2 className="title">บทความ : {title}</h2>
+            <img src={image} alt={title}></img>
+            <div>
+                <strong>ผู้เขียน : {author}</strong>
+                <p>{content}</p>
+            </div>
        </div>
     )
 }
