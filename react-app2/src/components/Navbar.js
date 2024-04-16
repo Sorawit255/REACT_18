7156ae6ceab10410aa95ecf6ae02f03c9e1c670d
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 21:40:58 +0700

    Navbar Style

--- react-app2/src/components/Navbar.js
+++ react-app2/src/components/Navbar.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
-
+import "./Navbar.css"
 export default function Navber(){
     return(
         <nav>
-            <Link to="/">Blogs Application</Link>
+            <Link to="/"><h3>Blogs Application</h3></Link>
             <Link to="/">หน้าแรก</Link>
             <Link to="/about">เกี่ยวกับเรา</Link>
             <Link to="/blogs">บทความทั้งหมด</Link>
