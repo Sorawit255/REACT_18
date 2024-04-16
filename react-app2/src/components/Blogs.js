a409e3cb5380b08c961a3cbe76131197d5741a8c
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 22:49:00 +0700

    ช่องค้นหาข้อมูล

--- react-app2/src/components/Blogs.js
+++ react-app2/src/components/Blogs.js
@@ -1,9 +1,20 @@
 import blogs from "../data/blogs";
 import "./Blogs.css"
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 export default function Blogs() {
+  const [search, setSearch] = useState("");
   return (
     <div className="blogs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
       <article>
         {blogs.map((item) => (
           <Link to={`/blog/${item.id}`} key={item.id}>
