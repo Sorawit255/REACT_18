4559922cde7098e9a701c1bf0d07cbd55ab44951
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 22:35:56 +0700

    ดึงข้อมูลบทความ

--- react-app2/src/components/Details.js
+++ react-app2/src/components/Details.js
@@ -1,6 +1,13 @@
 import { useParams } from "react-router-dom";
+import { useEffect ,useState} from "react"
+import blogs from "../data/blogs";
 export default function Details(){
     const {id} = useParams()
+    useEffect(()=>{
+        //ดึงข้อมูลบทความ
+        const result = blogs.find((item)=>item.id === parseInt(id))
+        console.log(result)
+    })
     return(
        <div className="container">
             <h2 className="title">บทความ : {id}</h2>
