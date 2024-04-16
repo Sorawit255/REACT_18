413ce54dcb839e1994ba0109cfee7138c1892564
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 22:38:54 +0700

    สร้าง State เก็บข้อมูล

--- react-app2/src/components/Details.js
+++ react-app2/src/components/Details.js
@@ -3,10 +3,17 @@ import { useEffect ,useState} from "react"
 import blogs from "../data/blogs";
 export default function Details(){
     const {id} = useParams()
+    const [title,setTitle] = useState("");
+    const [image,setImage] = useState("");
+    const [content,setContent] = useState("");
+    const [author,setAuthor] = useState("");
     useEffect(()=>{
         //ดึงข้อมูลบทความ
         const result = blogs.find((item)=>item.id === parseInt(id))
-        console.log(result)
+        setTitle(result.title)
+        setImage(result.image_url)
+        setContent(result.content)
+        setAuthor(result.author)
     })
     return(
        <div className="container">
