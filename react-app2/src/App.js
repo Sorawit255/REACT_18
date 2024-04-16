50c218c2e6d40dddf581b7d9c90da60be073804e
Sorawit255 <65030255@kmitl.ac.th>
2024-04-16 21:16:40 +0700

    create React-app2

---
+++ react-app2/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Blogs from "./components/Blogs";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
